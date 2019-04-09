b5053734cb90f954cd5665821dd8256869ac5e09
Alyssa Wei <wei.alyssa@gmail.com>
2019-04-08 18:45:37 -0700

    Completed function for stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,38 @@
 
 // but you don't so you're going to write it from scratch:
 
+//var obj = {a:1, b:2, c:3}
+//output = "{\"a\":1,\"b\":2,\"c\":3}"
+//output = "{\"a\":1,\"b\":2,\"c\": { \"d\": {} } }"
+
+//{'foo': true, 'bar': false, 'baz': null},
+
+
+//var obj = [8, [[], 3, 4]],
+// output = "[8,[[],3,4]]"
+
+
+
 var stringifyJSON = function(obj) {
-  //Testing liveShare
+  if (typeof obj === 'boolean'){return "" + obj}
+  if (typeof obj === 'number'){return "" + obj}
+  if (typeof obj === 'string'){return `"${obj}"`}
+  if (obj === null){return "" + null}
+  if (typeof obj === 'object'){
+
+    var output = [];
+    for (var key in obj){
+      var currKey = key;
+      var currVal = obj[key];
+
+      if (Array.isArray(obj)){
+        output.push(stringifyJSON(currVal))
+      }else{
+        output.push(`${stringifyJSON(currKey)}:${stringifyJSON(currVal)}`);
+      }
+    }
+    if (Array.isArray(obj)){return `[${output}]`}
+    if (!Array.isArray(obj)){return `{${output}}`}
+  }
 };
+
